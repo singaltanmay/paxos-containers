5681cdf10b42eeacd9133b68548caeaabea844e0
Tanmay Singal <tanmaysingal@icloud.com>
2022-11-19 21:12:08 -0800

    Added gitignore

---
+++ src/main/java/edu/sjsu/Main.java
@@ -0,0 +1,10 @@
+package edu.sjsu;/**
+* @created ${DAY}/${MONTH}/${YEAR}
+* @project ${PROJECT_NAME}
+* @author Tanmay Singal
+* @sjsu_id 016681795
+*/public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
