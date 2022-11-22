4d9fb92d1cab46c472e8d23f68e86377e67c50fe
Tanmay Singal <tanmaysingal@icloud.com>
2022-11-22 00:31:43 -0800

    Register with corrent role

--- src/main/java/edu/sjsu/Application.java
+++ src/main/java/edu/sjsu/Application.java
@@ -22,50 +22,52 @@ import java.util.Random;
 @SpringBootApplication
 public class Application {
 
-    public static PAXOS_ROLES applicationPaxosRole;
+  public static PAXOS_ROLES applicationPaxosRole;
 
-    public static String ROUTER_BASE_URL;
+  public static String ROUTER_BASE_URL;
 
-    public static void main(String[] args) {
-        ConfigurableApplicationContext applicationContext = SpringApplication.run(Application.class, args);
+  public static void main(String[] args) {
+    ConfigurableApplicationContext applicationContext = SpringApplication.run(Application.class, args);
 
-        String paxosRoleArg = System.getProperty("role");
-        PAXOS_ROLES[] paxosRoles = PAXOS_ROLES.values();
-        if (paxosRoleArg != null && !paxosRoleArg.isBlank()) {
-            for (PAXOS_ROLES paxosRole : paxosRoles) {
-                if (paxosRole.name().equals(paxosRoleArg.toUpperCase())) {
-                    applicationPaxosRole = paxosRole;
-                    break;
-                }
-            }
+    String paxosRoleArg = System.getProperty("role");
+    PAXOS_ROLES[] paxosRoles = PAXOS_ROLES.values();
+    if (paxosRoleArg != null && !paxosRoleArg.isBlank()) {
+      for (PAXOS_ROLES paxosRole : paxosRoles) {
+        if (paxosRole.name().equals(paxosRoleArg.toUpperCase())) {
+          applicationPaxosRole = paxosRole;
+          break;
         }
-        if (applicationPaxosRole == null) applicationPaxosRole = paxosRoles[new Random().nextInt(paxosRoles.length)];
-        System.out.println("Application started and assumed the role of " + applicationPaxosRole);
-
-        // Register with router as soon as application is started
-        Retrofit retrofit = RetrofitClient.getRetrofitInstance();
-        Router router = retrofit.create(Router.class);
-        Call<Void> call = router.register(new Register(PAXOS_ROLES.PROPOSER));
-        call.enqueue(new Callback<>() {
-            @Override
-            public void onResponse(Call<Void> call, Response<Void> response) {
-                System.out.println("Registered with code : " + response.code());
-                Proposer proposer = new Proposer();
-                proposer.sendPrepareMessage("Dog");
-            }
-
-            @Override
-            public void onFailure(Call<Void> call, Throwable throwable) {
-                System.err.println("Failed to register with the router at "+ call.request().url() +". Shutting down.");
-                SpringApplication.exit(applicationContext);
-            }
-        });
+      }
     }
-
-    @Value("${router.baseurl}")
-    private void setRouterBaseUrl(String baseUrl) {
-        Application.ROUTER_BASE_URL = baseUrl;
+    if (applicationPaxosRole == null) {
+      applicationPaxosRole = paxosRoles[new Random().nextInt(paxosRoles.length)];
     }
+    System.out.println("Application started and assumed the role of " + applicationPaxosRole);
 
-    public enum PAXOS_ROLES {PROPOSER, ACCEPTOR, LEARNER}
+    // Register with router as soon as application is started
+    Retrofit retrofit = RetrofitClient.getRetrofitInstance();
+    Router router = retrofit.create(Router.class);
+    Call<Void> call = router.register(new Register(applicationPaxosRole));
+    call.enqueue(new Callback<>() {
+      @Override
+      public void onResponse(Call<Void> call, Response<Void> response) {
+        System.out.println("Registered with code : " + response.code());
+        Proposer proposer = new Proposer();
+        proposer.sendPrepareMessage("Dog");
+      }
+
+      @Override
+      public void onFailure(Call<Void> call, Throwable throwable) {
+        System.err.println("Failed to register with the router at " + call.request().url() + ". Shutting down.");
+        SpringApplication.exit(applicationContext);
+      }
+    });
+  }
+
+  @Value("${router.baseurl}")
+  private void setRouterBaseUrl(String baseUrl) {
+    Application.ROUTER_BASE_URL = baseUrl;
+  }
+
+  public enum PAXOS_ROLES {PROPOSER, ACCEPTOR, LEARNER}
 }
